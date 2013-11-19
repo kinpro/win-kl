25b3cda916f2e027b6d4daaad9ba5f9d7bae7c72
JohnWassilak <john@wassilak.com>
2013-11-19 10:44:36 -0600

    Project initialization

---
+++ win-kl-java/src/main/java/com/wassilak/win_kl/kl/KL.java
@@ -0,0 +1,11 @@
+package com.wassilak.win_kl.kl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jwassilak
+ * Date: 11/15/13
+ * Time: 3:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface KL {
+}
